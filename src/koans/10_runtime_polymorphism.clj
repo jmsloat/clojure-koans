8cce594a882e852c7d530a7c44d96c8b4dad24d2
James Sloat <james.sloat@lsrlaw.com>
2017-11-30 15:57:46 -0500

    finished a few more sections

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -11,8 +11,8 @@
 
 (defmulti diet (fn [x] (:eater x)))
 (defmethod diet :herbivore [a] (str (get a :name) " eats veggies."))
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :carnivore [a] (str (get a :name) " eats animals."))
+(defmethod diet :default [a] (str "I don't know what " (get a :name) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
